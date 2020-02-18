38e53ddec7f20a4e2ab2b4eb742b6efc62a77c2b
prashant-raghu <prashantraghu999@gmail.com>
2020-02-19 02:40:11 +0530

    helper added

--- index.js
+++ index.js
@@ -24,30 +24,7 @@ const package = `{
     "dependencies": {
     }
 }`
-function node_mod() {
-    fs.writeFile(__dirname + "/static/package.json", package, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    });
-    exec('cd ./static ; npm i express', function (err, stdout, stderr) {
-        console.log('Done: installed node modules')
-    })
-}
-function start() {
-    var child = spawn(process.execPath, ['static/sample.js', 'massage'])
-    setTimeout(() => {
-        child.kill('SIGINT')
-    }, 1000)
-    child.stdout.on('data', function (data) {
-        console.log('from child: ', data.toString())
-    })
-    child.on('exit', function (code, signal) {
-        if (code != 0)
-            console.log('child process exited with ' + `code ${code} and signal ${signal}`)
-    });
-}
+
 function extractRequire(code) {
     var codeSplit = code.replace(/\n/g, " ").split(" ")
     var res = [];
